b9c779e4a9b23cb0473d7eb22b362e74a0052083
Design Tech TI <mrc_490@hotmail.com>
2020-06-21 17:59:16 -0300

    Operações matemáticas
    Com nº inteiros

--- hello world.py
+++ hello world.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*-
-print ("Olá mundo")
+
+# Operações matemáticas
+
+#Soma
+print (2 + 2)
+#Subtração
+print (2 - 2)
+#Multiplicação
+print (2 * 2)
+#Divisão
+print (2 / 2)
+#Exponenciação
+print (2 ** 2)
+#Módulos
+print (10 % 3)
