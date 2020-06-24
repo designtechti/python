7ed0cefffcb7deb84dddb92b5f3daeb02db1d59d
Design Tech TI <mrc_490@hotmail.com>
2020-06-24 18:51:23 -0300

    Estruturas condicionais
    Comando else

--- hello world.py
+++ hello world.py
@@ -8,18 +8,6 @@ y = 3
 if x > y:
 	print("x é maior que y")
 
-if x < y:
+else: 
 	print("x é menor que y")
-
-if x == y:
-	print("x é igual à y")
-
-if x != y:
-	print("x é maior à y")
-
-if x >= y:
-	print("x é maior ou igual à y")
-
-if x <= y:
-	print("x é menor ou igual à y")
 
