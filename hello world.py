1540295aee44f616b974e0f28669c017bf482c2d
Design Tech TI <mrc_490@hotmail.com>
2020-06-27 15:08:23 -0300

    Python 2 vs. Python 3

--- hello world.py
+++ hello world.py
@@ -1,17 +1,28 @@
 # -*- coding: utf-8 -*-
 
-# Tratamento de exceções #
+# Python 2 vs Python 3 #
 
-a = 2
-b = 0
+""" 
+	1) COMANDO print()
+		Em Python 3 passa ser obrigatório o uso de parênteses. Veja:
+		- print()
 
-try:
-	print(a/b)
-except:
-	print("Não é possível divisão por 0") 
+	2) COMANDO input()
+		Em Python 2 há duas variações:
+		
+		# strings
+		- raw_input() 
 
-""" Tenta fazer uma operação matemática 
-	e caso não der, emite uma mensagem 
-	de erro para poder continuar
-	a execução do programa.
+		# valores numéricos
+		- input()
+
+		Já em Python 3, deve-se usar apenas input() para strings, e para 
+		números  deve-se combinar com as funções int e float. Veja: 
+		
+		# Recebendo textos
+		- meu_texto = input("Digite um texto: ")
+
+		# Recebendo números
+		- numero_inteiro = int(input("Digite um número inteiro: "))
+		- numero_decimal = float(input("Digite um número decimal: "))
 """
