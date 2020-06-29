c52f0434fbb84e90cfa40aedb2d865d1d8049bd0
Design Tech TI <mrc_490@hotmail.com>
2020-06-29 16:29:19 -0300

    Update exercício 5

--- exercicios/exercicio-5.py
+++ exercicios/exercicio-5.py
@@ -1,43 +1,30 @@
 # -*- coding: utf-8 -*-
 
-# EXERCÍCIO 5 - operacao MATEMÁTICA #
+# EXERCÍCIO 5 - operacaoERAÇÃO MATEMÁTICA #
 
 # Definindo variáveis
 
-n1 = int(input("Digite o 1º número: \n"))
-n2 = int(input("Digite o 2º número: \n"))
-operacao = input("Digite o sinal da operacao que deseja realizar? \n" + "(+) Adição;	(-) Subtração;	(x) Multiplicação;	(/) Divisão;	(^) Exponenciação;	\n")
+n1 = int(input("Digite o primeiro número: "))
+sinal = input("Digite o sinal: ")
+n2 = int(input("Digite o segundo número: "))
+	 
+# Definindo condições
 
-# Definindo funções
-
-def soma(n1, n2):
-	return n1 + n2
-
-def subtracao(n1, n2):
-	return n1 - n2
-
-def multiplicacao(n1, n2):
-	return n1 * n2
-
-def divisao(n1, n2):
-	return n1 / n2
-
-def exponenciacao(n1, n2):
-	return n1 ** n2
-
-# Define as condições
-
-if operacao == "+":
-	print(soma(n1,n2))
-
-elif operacao == "-":
-	print(subtracao(n1,n2))
-
-elif operacao == "x":
-	print(multiplicacao(n1,n2))
-
-elif operacao == "/":
-	print(divisao(n1,n2))
-
+if sinal == "+":
+	operacao = n1 + n2
+ 
+elif sinal == "-":
+	operacao = n1 - n2
+ 
+elif sinal == "/":
+	operacao = n1 + n2
+ 
+elif sinal == "*":
+	operacao = n1 * n2
+ 
 else:
-	print(exponenciacao(n1,n2))
+	print("Sinal inválido.")
+
+# Exibe na tela o resultado
+	 
+print(operacao)
